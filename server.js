fcf8320304e5e7d1253d471f27d91a9a9f41c6dc
unknown <bikramzmail323@gmail.com>
2025-06-23 16:47:42 -0500

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views", "pug"));
+app.set('views', './views/pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
