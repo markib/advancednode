e78d113ea3dac52437d5104539a0e116f9133608
unknown <bikramzmail323@gmail.com>
2025-06-23 16:01:18 -0500

    first commit

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+const path = require("path");
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views", "pug"));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render("index", {
+    title: "Welcome to FCC",
+    message: "Pug is working!",
+    showLogin: true,
+    showRegistration: true,
+    showSocialAuth: true,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
