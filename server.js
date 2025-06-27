c5b86c52518b65cc31e41efe337bed7abdba7c33
unknown <bikramzmail323@gmail.com>
2025-06-27 16:22:15 -0500

    Change path

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.set('view engine', 'pug');
-app.set('views', './views/pug');
+// app.set('views', './views/pug');
+app.set('views', path.join(__dirname, 'views', 'pug'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
